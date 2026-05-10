fbb328e003d4b798dcdde5d9c9b3cc6627bbb66d
slpagano-lang <slpagano@gmail.com>
2026-05-10 18:07:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    base: '/movilsat-web/',
+    plugins: [react()],
+    optimizeDeps: {
+        exclude: ['lucide-react'],
+    },
 });
